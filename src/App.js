63fe8af1544bb472bcb5614c23c2d36a3c0c860f
cyberstizz <charles.lamb.dev@gmail.com>
2021-03-14 21:12:00 -0400

    added the decorative div and skewed the x axis 30 degrees! I wanna kiss it!

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="decorativeDiv"></div>
       <div className="navBar">
     
     <div className="logo"></div>
