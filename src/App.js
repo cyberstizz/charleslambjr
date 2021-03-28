7b000030fb3666dbe11b64a2080a23c54af2928d
Charles Lamb <charles.lamb.dev@gmail.com>
2021-03-28 16:47:41 -0400

    got rid of icon for social media button mess. It was destroying the integrity of the page

--- src/App.js
+++ src/App.js
@@ -22,21 +22,7 @@ function App() {
     <div className="leftFoldSection">
       <div className="socialMediaButtons">
 
-      <div class="footer-social-icons">
-    <h4 class="_14">Follow me on</h4>
-    <ul class="social-icons">
-   
-    <li><a href="" class="social-icon">
- <i class="fa fa-twitter"></i></a></li>
-
-    <li><a href="" class="social-icon"> 
- <i class="fa fa-youtube"></i></a></li>
-    <li><a href="" class="social-icon"> 
- <i class="fa fa-linkedin"></i></a></li>
-    <li><a href="" class="social-icon"> 
- <i class="fa fa-github"></i></a></li>
-    </ul>
-</div>
+      
 
 
       </div>
