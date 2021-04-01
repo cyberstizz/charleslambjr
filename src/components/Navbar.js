0bb4286786c3deb644c27508e2bdc25c98945acd
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-01 18:21:13 -0400

    broke the entiiire navbar into one module and imported it. Viola! its working.

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import '../App.css';
+
+
+const Navbar = () =>{
+    return(
+
+        <div className="navBar">
+      <div className="logo"></div>
+
+    <div className="navOptions">
+      
+   <div className="home">Home</div>
+   <div className="projects">Projects</div>
+   <div className="challenges">Challenges</div>
+   <div className="about">About</div>
+   <div className="lamb">Lamb</div>
+   <div className="journal">Journal</div>
+  
+    </div>
+
+      </div>
+    )
+};
+
+export default Navbar;
