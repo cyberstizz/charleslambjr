9325f68dd06e0c4298deede655dfdab60753fa3b
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-02 21:44:10 -0400

    created a new module called card that I will use as functional components to display the card. the component takes props as a parameter

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Card.css';
+
+
+const Card = (props) => {
+    
+}
+
+export default Card;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,10 +5,13 @@ import './Projects.css'
 const Projects = () =>{
 return (
     <React.Fragment>
-    <div className="projectsBackground"></div>
+        <div id="projectsBackground">
+
+    <div className="projectsNavDecoratio"></div>
         <div className="projectsTitle">Projects</div>
-        <div className="projectsInfoDiv">These are a few projects I have done recently</div>
+        <div className="projectsInfoDiv">These are a few projects I have completed recently</div>
         <div id="cardsDiv"></div>
+        </div>
         </React.Fragment>
 )
 }
