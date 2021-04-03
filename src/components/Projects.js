644df2ff2a27a060531e705441c51e36a37ec92e
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-02 22:29:11 -0400

    added a few properties to the state of the projects component but still need to add a couple more

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,20 @@ import './Projects.css';
 import Card from './Card';
 
 class Projects extends React.Component{
-    
+    constructor(props){
+        super(props);
+
+        this.state ={
+            cardOneTitle: 'Squint',
+            cardTwoTitle: 'KnowIt',
+            cardThreeTitle: 'Quickshow',
+            cardFourTitle: 'random',
+            cardOneDescription: 'A goal application allowing users to sign up, enter their goals and aspirations and track them daily',
+            cardTwoDescription: 'A flashcard application based on the popular scientific study of spaced repetiton',
+            cardThreeDescription: 'An application using the shows Api that allows users to find out information about their favorite shows and when they first aired quickly',
+            cardFourDescription: 'random'
+        }
+    }
 
     render(){
 return (
@@ -15,7 +28,7 @@ return (
         <div className="projectsTitle">Projects</div>
         <div className="projectsInfoDiv">These are a few projects I have completed recently</div>
         <div id="cardsDiv">
-        <Card /> <Card /> <br/><br/>
+        <Card title={this.state.cardOneTitle} description={this.state.cardOneDescription}/> <Card /> <br/><br/>
         <Card /> <Card />
 
         </div>
