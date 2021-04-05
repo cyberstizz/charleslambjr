3e19d27da217d217f67d5171c2ec6b2c11375446
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-04 20:48:27 -0400

    added the about header but it is too small for now, I will play with the font size now

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ return (
     <div className="aboutPage">
         <div id="aboutTopDecoration"></div>
         <div id='aboutSkewedDov'></div>
-        <div id="aboutHeader"></div>
+        <div id="aboutHeader">About</div>
         <div id="aboutParagraph"></div>
         <div id="skillsSection"></div>
         <div id="aboutFooter"></div>
