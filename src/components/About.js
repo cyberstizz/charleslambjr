241341db7cebe57b7bc43e60e8ab90e0846fea17
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-04 22:48:36 -0400

    changed the text div to absolute because it was being covered by the skewed div and I spent about ten minutes truing to figure out why

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,10 @@ return (
         <div id="aboutTopDecoration"></div>
         <div id='aboutSkewedDov'></div>
         <div id="aboutHeader">About</div>
-        <div id="aboutParagraph"></div>
+        <div id="aboutParagraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem. </div>
         <div id="skillsSection"></div>
         <div id="aboutFooter"></div>
     </div>
