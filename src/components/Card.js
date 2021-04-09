351ff8b2564a7a2447048af5b8a0702957104c96
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-09 11:56:23 -0400

    updated the state to add a color for the buttons and added them through inline styles and props

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,8 @@ import './Card.css';
 const Card = (props) => {
     return (
         <div id='wholeCard'>
-            <div id='cardTop' style={{backgroundColor: props.backgroundColor}}><br />  {props.title} <br /> <button style={{backgroundColor: props.backgroundColor, 'color': 'white', 'font-size': '8px', 'font-family': 'Permanent Marker, cursive',
-width: '5vw', height: '3vh'}}>Preview</button></div>
+            <div id='cardTop' style={{backgroundColor: props.backgroundColor}}><br />  {props.title} <br /> <button style={{backgroundColor: props.buttonColor, 'font-size': '8px', 'font-family': 'Permanent Marker, cursive',
+width: '7vw', height: '5vh', marginRight: '.5vw', color: 'white'}}>Preview</button></div>
         <div id="entireCardBody">
         <div id="cardTitle"></div>
         </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,12 +9,16 @@ class Projects extends React.Component{
 
         this.state ={
             cardOneTitle: 'Squint',
+            cardOneButtonColor: 'rgb(0, 34, 102)',
             cardOneTopColor: 'rgb(249, 140, 31)',
             cardTwoTitle: 'KnowIt',
+            cardTwoButtonColor: 'rgb(249, 140, 31)',
             cardTwoTopColor: 'rgb(0, 34, 102)',
             cardThreeTitle: 'Quickshow',
+            cardThreeButtonColor: 'rgb(0, 34, 102)',
             cardThreeTopColor: 'rgb(249, 140, 31)',
             cardFourTitle: 'Wizard vs Witch',
+            cardFourButtonColor: 'rgb(249, 140, 31)',
             cardFourTopColor: 'rgb(0, 34, 102)',
             cardOneDescription: 'A goal application allowing users to sign up, enter their goals and aspirations and track them daily',
             cardTwoDescription: 'A flashcard application based on the popular scientific study of spaced repetiton',
@@ -32,8 +36,8 @@ return (
         <div className="projectsTitle">Projects</div>
         <div className="projectsInfoDiv">These are a few projects I have completed recently</div>
         <div id="cardsDiv">
-        <Card title={this.state.cardOneTitle} backgroundColor={this.state.cardOneTopColor} description={this.state.cardOneDescription} /> <Card title={this.state.cardTwoTitle} description={this.state.cardTwoDescription} backgroundColor={this.state.cardTwoTopColor} /> <br/><br/><n/>
-        <Card title={this.state.cardThreeTitle} description={this.state.cardThreeDescription} backgroundColor={this.state.cardThreeTopColor} /> <Card title={this.state.cardFourTitle} description={this.state.cardFourDescription} backgroundColor={this.state.cardFourTopColor} />
+        <Card title={this.state.cardOneTitle} buttonColor={this.state.cardOneButtonColor} backgroundColor={this.state.cardOneTopColor} description={this.state.cardOneDescription} /> <Card title={this.state.cardTwoTitle} buttonColor={this.state.cardTwoButtonColor} description={this.state.cardTwoDescription} backgroundColor={this.state.cardTwoTopColor} /> <br/><br/><n/>
+        <Card title={this.state.cardThreeTitle} buttonColor={this.state.cardThreeButtonColor} description={this.state.cardThreeDescription} backgroundColor={this.state.cardThreeTopColor} /> <Card title={this.state.cardFourTitle} description={this.state.cardFourDescription} buttonColor={this.state.cardFourButtonColor} backgroundColor={this.state.cardFourTopColor} />
 
         </div>
         </div>
