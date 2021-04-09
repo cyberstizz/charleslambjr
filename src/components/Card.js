2edd323fe07183a3387de49a74e603bd05aedbcd
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-09 11:38:58 -0400

    added a button to the top of the cards and styled it a bit though I need more

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,8 @@ import './Card.css';
 const Card = (props) => {
     return (
         <div id='wholeCard'>
-            <div id='cardTop' style={{backgroundColor: props.backgroundColor}}><br />  {props.title} <br /> <button style={{width: '4vw', height: '3vh'}}>Preview</button></div>
+            <div id='cardTop' style={{backgroundColor: props.backgroundColor}}><br />  {props.title} <br /> <button style={{backgroundColor: props.backgroundColor, 'color': 'white', 'font-size': '8px', 'font-family': 'Permanent Marker, cursive',
+width: '5vw', height: '3vh'}}>Preview</button></div>
         <div id="entireCardBody">
         <div id="cardTitle"></div>
         </div>
