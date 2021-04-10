9feec6f867d89dbc277109a58a2f71a24d30cc59
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-10 16:33:24 -0400

    added the screenshots for all of the projects on the projects page including knowit

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ const Card = (props) => {
         <div id='cardTop' style={{backgroundColor: props.backgroundColor}}><br />  {props.title} <br /> <a href='https://squint.netlify.app/'><button style={{backgroundColor: props.buttonColor, 'font-size': '18px', 'font-family': 'Permanent Marker, cursive',
 width: '7vw', height: '5vh', marginRight: '.5vw', color: props.backgroundColor}}>Preview</button></a></div>
         <div id="entireCardBody">
-        <div id="cardTitle" style={{backgroundImage: props.backgroundImage, backgroundSize: '100%', height: '40vh', width: '20vw', backgroundPosition: 'contain', backgroundRepeat: 'no-repeat'}}></div>
+        <div id="cardTitle" style={{backgroundImage: props.backgroundImage, backgroundSize: '100%', height: '400px',minWidth: '100%', backgroundPosition: 'contain', backgroundSize: 'contain', backgroundRepeat: 'no-repeat'}}></div>
         </div>
         </div>
     )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,9 @@ import '../App.css';
 import './Projects.css';
 import Card from './Card';
 import background from "../squintScreenshot.png";
+import quickShowBackground from "../quickShowScreenshot.png";
+import knowitBackground from "../knowitscreenshot.png";
+import wizardVsWitchBackground from "../WizardvsWitchScreenshot.png";
 
 class Projects extends React.Component{
     constructor(props){
@@ -14,12 +17,15 @@ class Projects extends React.Component{
             cardOneButtonColor: 'rgb(0, 34, 102)',
             cardOneTopColor: 'rgb(249, 140, 31)',
             cardTwoTitle: 'KnowIt',
+            cardTwoBackgroundImage: `url(${quickShowBackground})`,
             cardTwoButtonColor: 'rgb(249, 140, 31)',
             cardTwoTopColor: 'rgb(0, 34, 102)',
             cardThreeTitle: 'Quickshow',
+            cardThreeBackgroundImage: `url(${wizardVsWitchBackground})`,
             cardThreeButtonColor: 'rgb(0, 34, 102)',
             cardThreeTopColor: 'rgb(249, 140, 31)',
             cardFourTitle: 'Wizard vs Witch',
+            cardFourBackgroundImage: `url(${knowitBackground})`,
             cardFourButtonColor: 'rgb(249, 140, 31)',
             cardFourTopColor: 'rgb(0, 34, 102)',
             cardOneDescription: 'A goal application allowing users to sign up, enter their goals and aspirations and track them daily',
@@ -38,8 +44,8 @@ return (
         <div className="projectsTitle">Projects</div>
         <div className="projectsInfoDiv">These are a few projects I have completed recently</div>
         <div id="cardsDiv">
-        <Card title={this.state.cardOneTitle}  backgroundImage={this.state.cardOneBackgroundImage} buttonColor={this.state.cardOneButtonColor} backgroundColor={this.state.cardOneTopColor} description={this.state.cardOneDescription} /> <Card title={this.state.cardTwoTitle} buttonColor={this.state.cardTwoButtonColor} description={this.state.cardTwoDescription} backgroundColor={this.state.cardTwoTopColor} /> <br/><br/><n/>
-        <Card title={this.state.cardThreeTitle} buttonColor={this.state.cardThreeButtonColor} description={this.state.cardThreeDescription} backgroundColor={this.state.cardThreeTopColor} /> <Card title={this.state.cardFourTitle} description={this.state.cardFourDescription} buttonColor={this.state.cardFourButtonColor} backgroundColor={this.state.cardFourTopColor} />
+        <Card title={this.state.cardOneTitle}  backgroundImage={this.state.cardOneBackgroundImage} buttonColor={this.state.cardOneButtonColor} backgroundColor={this.state.cardOneTopColor} description={this.state.cardOneDescription} /> <Card title={this.state.cardTwoTitle} buttonColor={this.state.cardTwoButtonColor} backgroundImage={this.state.cardFourBackgroundImage} description={this.state.cardTwoDescription} backgroundColor={this.state.cardTwoTopColor} /> <br/><br/><n/>
+        <Card title={this.state.cardThreeTitle} backgroundImage={this.state.cardTwoBackgroundImage} buttonColor={this.state.cardThreeButtonColor} description={this.state.cardThreeDescription} backgroundColor={this.state.cardThreeTopColor} /> <Card title={this.state.cardFourTitle} description={this.state.cardFourDescription} backgroundImage={this.state.cardThreeBackgroundImage} buttonColor={this.state.cardFourButtonColor} backgroundColor={this.state.cardFourTopColor} />
 
         </div>
         </div>
