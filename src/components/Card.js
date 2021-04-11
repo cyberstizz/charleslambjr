c90c99794792b3071b349386e67aca3a25c470e6
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-11 00:17:05 -0400

    added all of the links for the projects individual sites on netlify in the projects components state and passed it as props to the card component so it can display in its respective project

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ import './Card.css';
 const Card = (props) => {
     return (
         <div id='wholeCard'>
-        <div id='cardTop' style={{backgroundColor: props.backgroundColor}}><br />  {props.title} <br /> <a href='https://squint.netlify.app/'><button style={{backgroundColor: props.buttonColor, 'font-size': '18px', 'font-family': 'Permanent Marker, cursive',
+        <div id='cardTop' style={{backgroundColor: props.backgroundColor}}><br />  {props.title} <br /> <a href={props.link}><button style={{backgroundColor: props.buttonColor, 'font-size': '18px', 'font-family': 'Permanent Marker, cursive',
 width: '7vw', height: '5vh', marginRight: '.5vw', color: props.backgroundColor}}>Preview</button></a></div>
         <div id="entireCardBody">
         <div id="cardTitle" style={{backgroundImage: props.backgroundImage, backgroundSize: '100%', height: '400px',minWidth: '100%', backgroundPosition: 'contain', backgroundSize: 'contain', backgroundRepeat: 'no-repeat'}}></div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,24 +10,33 @@ import wizardVsWitchBackground from "../WizardvsWitchScreenshot.png";
 class Projects extends React.Component{
     constructor(props){
         super(props);
+        const firstLink = 'https://squint.netlify.app';
+            
+        const secondLink = 'https://wizardvswitch.netlify.app';
+        const thirdLink = 'https://quickshow.netlify.app';
+        const fourthLink = 'https://wizardvswitch.netlify.app';
 
         this.state ={
             cardOneTitle: 'Squint',
             cardOneBackgroundImage: `url(${background})`,
             cardOneButtonColor: 'rgb(0, 34, 102)',
             cardOneTopColor: 'rgb(249, 140, 31)',
+            cardOneLink: firstLink,
             cardTwoTitle: 'KnowIt',
             cardTwoBackgroundImage: `url(${quickShowBackground})`,
             cardTwoButtonColor: 'rgb(249, 140, 31)',
             cardTwoTopColor: 'rgb(0, 34, 102)',
+            cardTwoLink: secondLink,
             cardThreeTitle: 'Quickshow',
             cardThreeBackgroundImage: `url(${wizardVsWitchBackground})`,
             cardThreeButtonColor: 'rgb(0, 34, 102)',
             cardThreeTopColor: 'rgb(249, 140, 31)',
+            cardThreeLink: thirdLink,
             cardFourTitle: 'Wizard vs Witch',
             cardFourBackgroundImage: `url(${knowitBackground})`,
             cardFourButtonColor: 'rgb(249, 140, 31)',
             cardFourTopColor: 'rgb(0, 34, 102)',
+            cardFourLink: fourthLink,
             cardOneDescription: 'A goal application allowing users to sign up, enter their goals and aspirations and track them daily',
             cardTwoDescription: 'A flashcard application based on the popular scientific study of spaced repetiton',
             cardThreeDescription: 'An application using the shows Api that allows users to find out information about their favorite shows and when they first aired quickly',
@@ -44,8 +53,8 @@ return (
         <div className="projectsTitle">Projects</div>
         <div className="projectsInfoDiv">These are a few projects I have completed recently</div>
         <div id="cardsDiv">
-        <Card title={this.state.cardOneTitle}  backgroundImage={this.state.cardOneBackgroundImage} buttonColor={this.state.cardOneButtonColor} backgroundColor={this.state.cardOneTopColor} description={this.state.cardOneDescription} /> <Card title={this.state.cardTwoTitle} buttonColor={this.state.cardTwoButtonColor} backgroundImage={this.state.cardFourBackgroundImage} description={this.state.cardTwoDescription} backgroundColor={this.state.cardTwoTopColor} /> <br/><br/><n/>
-        <Card title={this.state.cardThreeTitle} backgroundImage={this.state.cardTwoBackgroundImage} buttonColor={this.state.cardThreeButtonColor} description={this.state.cardThreeDescription} backgroundColor={this.state.cardThreeTopColor} /> <Card title={this.state.cardFourTitle} description={this.state.cardFourDescription} backgroundImage={this.state.cardThreeBackgroundImage} buttonColor={this.state.cardFourButtonColor} backgroundColor={this.state.cardFourTopColor} />
+        <Card title={this.state.cardOneTitle}  link={this.state.cardOneLink} backgroundImage={this.state.cardOneBackgroundImage} buttonColor={this.state.cardOneButtonColor} backgroundColor={this.state.cardOneTopColor} description={this.state.cardOneDescription} /> <Card title={this.state.cardTwoTitle} link={this.state.cardTwoLink} buttonColor={this.state.cardTwoButtonColor} backgroundImage={this.state.cardFourBackgroundImage} description={this.state.cardTwoDescription} backgroundColor={this.state.cardTwoTopColor} /> <br/><br/><n/>
+        <Card title={this.state.cardThreeTitle} link={this.state.cardThreeLink} backgroundImage={this.state.cardTwoBackgroundImage} buttonColor={this.state.cardThreeButtonColor} description={this.state.cardThreeDescription} backgroundColor={this.state.cardThreeTopColor} /> <Card title={this.state.cardFourTitle} link={this.state.cardFourLink} description={this.state.cardFourDescription} backgroundImage={this.state.cardThreeBackgroundImage} buttonColor={this.state.cardFourButtonColor} backgroundColor={this.state.cardFourTopColor} />
 
         </div>
         </div>
