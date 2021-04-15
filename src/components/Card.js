8a6e501f8706c7008083bea3b014f69507fba370
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-15 13:44:40 -0400

    fixed the error of added url to the links I put in the state. This only works if using the background image property

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = (props) => {
     return (
         <div id='wholeCard'>
         <div id='cardTop' style={{backgroundColor: props.backgroundColor}}><br />  {props.title} <br /> <a href={props.link}><button style={{backgroundColor: props.buttonColor, 'font-size': '18px', 'font-family': 'Permanent Marker, cursive',
-width: '7vw', height: '5vh', marginRight: '.5vw', color: props.backgroundColor}}>Preview</button></a><a href={props.link}><button style={{backgroundColor: props.buttonColor, 'font-size': '18px', 'font-family': 'Permanent Marker, cursive',
+width: '7vw', height: '5vh', marginRight: '.5vw', color: props.backgroundColor}}>Preview</button></a><a href={props.github}><button style={{backgroundColor: props.buttonColor, 'font-size': '18px', 'font-family': 'Permanent Marker, cursive',
 width: '7vw', height: '5vh', marginRight: '.5vw', color: props.backgroundColor}}>Github</button></a></div>
         <div id="cardBody" style={{backgroundImage: props.backgroundImage, backgroundSize: '100%', height: '40vh', width: '20vw', textAlign: 'center', position: 'relative', backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}></div>
         </div>
