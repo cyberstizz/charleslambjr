00a54f28db8254f8499764e25d867c0a621cd3d9
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-15 18:11:19 -0400

    fixed the description for the footer on the projects page

--- src/components/Challenges.js
+++ src/components/Challenges.js
@@ -4,9 +4,14 @@ import './Challenges.css';
 
 const Challenges = () =>{
 return (
+    <React.Fragment>
     <div id="fullPagePic">
         <div id="blueOverlay"></div>
+        <div id="title">My toughest Challenges</div>
     </div>
+    
+
+    </React.Fragment>
 )
 }
 
