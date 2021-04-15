a64a84aee86ddc027e17c07640beb438bbadc81d
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-15 19:07:07 -0400

    added the text for the description of the challenges page

--- src/components/Challenges.js
+++ src/components/Challenges.js
@@ -6,7 +6,17 @@ const Challenges = () =>{
 return (
     <React.Fragment>
                  <div id="title">My toughest Challenges</div>
-                 <div id="titleDescription"></div>
+                 <div id="titleDescription">One thing I really love is pushing myself to learn and grow!
+During my experience learning as a developer i have had many ups and downs which have taught me two important things:
+
+The learning never stops because the more I learn the more I realize I don’t know
+and
+I learn the most valuable lessons from my most annoying mistakes unfortunately.
+Below are a few of the most important coding challenges I have ever encountered
+Because they taught me either programming patterns I did not know or triggered paradigm shifts that changed how I Handled challenges in the future. Click the icons to view these challenges and/or there respective audio descriptions where I explain exactly how I overcame them<br/> <br/>
+Oh and in case you're wondering... yes that is me in the background overcoming one of my toughest physical challenges
+
+</div>
 
     <div id="fullPagePic">
         <div id="blueOverlay"></div>
