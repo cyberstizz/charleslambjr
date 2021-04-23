483a4de58f2e1c1d7b353dbbe92d23fe543c1821
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-22 20:29:59 -0400

    added a journal entry component to the journals page and an instance inside the journal component but this instance is still empty

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import './Journal.css';
 
 
 const Journal = () =>{
+
+const JournalEntry = (entry) =>{
+       return(
+              <div className='entryDiv'></div>
+       )
+}
+
 return(
-<div className="JournalPage" style={{width: '100vw', height: '100vh',backgroundColor: 'rgb(134, 193, 212)', textAlign: 'center', fontSize: '40px', color: 'white'}}><br/><br/><br/><br/><br/><br/> journal entries coming soon
-       
-       </div>
-)
+<JournalEntry entry={}/>
+       )
 }
 export default Journal;
