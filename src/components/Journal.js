0622e0329dfd7fc820e5199db863cd28457f82c9
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-22 21:00:07 -0400

    added background pic of me and my son but I still need to crop it as well as make styling changes to the journal entries div

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -2,16 +2,20 @@ import React from 'react';
 import './Journal.css';
 
 
-const Journal = () =>{
+const Journal = (props) =>{
 
-const JournalEntry = (entry) =>{
+const JournalEntry = (entry, postTime) =>{
        return(
-              <div className='entryDiv'></div>
+              <div id='entireJournalPage'>
+              <div className='entryDiv'>{props.entry}</div>
+              <div>posted on {props.postTime}</div>
+
+              </div>
        )
 }
 
 return(
-<JournalEntry entry={}/>
+<JournalEntry entry={'waterfountan'} postTime={'april 22nd'} />
        )
 }
 export default Journal;
