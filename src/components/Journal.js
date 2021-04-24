29aaa4abeea8e796cb5a88b6f281f657221a4d51
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-23 21:22:02 -0400

    created an if else to determine the month for my posts

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -17,7 +17,35 @@ if (hours > 12) {
    hours = 12;
 };
 
-const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+let month= today.getMonth()+1;
+
+if(month === 1){
+       month = 'January';
+} else if(month === 2){
+       month ='February';
+} else if(month === 3){
+       month = 'March';
+} else if(month === 4){
+       month = 'April';
+}else if(month === 5){
+       month = 'May';
+}else if(month === 6){
+       month = 'June';
+}else if(month === 7){
+       month = 'July';
+}else if(month === 8){
+       month = 'August';
+}else if(month === 9){
+       month = 'September';
+}else if(month === 10){
+       month = 'October';
+}else if(month === 11){
+       month = 'November';
+}else if(month === 12){
+       month = 'December';
+}
+
+const date = today.getFullYear()+'-'+month+'-'+today.getDate();
 const time = hours + ":" + today.getMinutes() + ":" + today.getSeconds();
 const dateTime = date+' '+time;
        return(
