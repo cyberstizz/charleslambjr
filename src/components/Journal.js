c49e80a464dfdfdfddeb7072142c368fcb69e717
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-23 21:11:38 -0400

    added the date object and some functions to add the date to each post without using anu props,rather used on initiation

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -4,18 +4,29 @@ import './Journal.css';
 
 const Journal = (props) =>{
 
-const JournalEntry = (entry, postTime) =>{
-       return(
-              <div id='entireJournalPage'>
-              <div className='entryDiv'>{props.entry}</div>
-              <div>posted on {props.postTime}</div>
+const JournalEntry = (props) =>{
 
+const today = new Date();
+const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+const dateTime = date+' '+time;
+       return(
+              <div className='entryDiv'>
+                     {props.entry}<br />
+              <div>posted on {dateTime}</div>
               </div>
+
        )
 }
 
 return(
+       <div id='entireJournalPage'>
+
+
 <JournalEntry entry={'waterfountan'} postTime={'april 22nd'} />
+
+</div>
+
        )
 }
 export default Journal;
