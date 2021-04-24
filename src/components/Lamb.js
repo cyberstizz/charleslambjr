1d2c11c5e5ce2df19d153058a9dd0928aa33b0ec
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-24 09:50:40 -0400

    added a white container that I will use to make the logo more readable and skew

--- src/components/Lamb.js
+++ src/components/Lamb.js
@@ -5,7 +5,13 @@ const Lamb = () =>{
 return (
     <div id="fullLambPage">
 
-        <div id='leftOrangeSide'></div>
+        <div id='leftOrangeSide'>
+            <div id='topSkewedDiv'></div>
+
+
+        </div>
+
+
         <div id='rightPicSide'></div>
 
        
