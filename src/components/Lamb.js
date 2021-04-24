26814cc79aef1297b0f04d690f858cd9d9c3bea3
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-24 08:54:30 -0400

    changed the div that I had as a placceholder on the lamb page and added a new id to it

--- src/components/Lamb.js
+++ src/components/Lamb.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Lamb.css'
 
 const Lamb = () =>{
 return (
-    <div className="aboutPage" style={{width: '100vw', height: '100vh',backgroundColor: 'navy', textAlign: 'center', fontSize: '40px', color: 'white'}}><br/><br/><br/><br/><br/><br/> Page coming soon
+    <div id="fullLambPage">
        
     </div>
 )
