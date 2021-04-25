bc38ac53ec5b3888acfc335487aa6e12198992f7
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-25 00:31:06 -0400

    added description text to the lamb page

--- src/components/Lamb.js
+++ src/components/Lamb.js
@@ -7,7 +7,8 @@ return (
 
         <div id='leftOrangeSide'>
             <div id='topSkewedDiv'></div>
-        <div id='lambDescription'>What is Lamb?</div>
+        <div id='lambDescriptionTitle'>What is Lamb?</div>
+        <div id='lambDesctiption'>Lamb Enterprises  is my way of giving back to the world through business endeavors, charitable pursuits, fundraisers and more. Our team is dedicated to help the world grow and be a better place. Lamb was Incorporated in 2021 with eight subsidiaries and 2 non profits, and we are committed to continued growth for the foreseeable future. You can learn more by visiting the Lamb website.</div>
 
         </div>
 
