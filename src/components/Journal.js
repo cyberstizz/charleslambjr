a8c432a4c9f50ad44c3a7ad6cacb2e568d3b4160
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-25 22:51:56 -0400

    added a journal entry for the day

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -63,6 +63,7 @@ const JournalEntry = (props) =>{
 return(
        <div id='entireJournalPage'>
 
+<JournalEntry dateTime={'April-25-2021 10:46pm'} entry={'Even though Sunday was mu off day, I learned a lot. The best part is that I finally solved an issue I was having for weeks with the challneges page, so my mood is great.'} />
 <JournalEntry dateTime={'April-24-2021 11:43pm'} entry={'Another incredible day. Besides hitting new highs on my shoulder press I have pretty much completed the Lamb page of this portfolio.tomorrows forecast... Sunday rest'} />
 <JournalEntry dateTime={'April-23-2021 9:45pm'} entry={'What a day! spent the majority of the day practicing the pern stack and good ol typing. tomorrows forecast, more work'} />
 <JournalEntry dateTime={dateTime} entry={'this is a test entry'} />
