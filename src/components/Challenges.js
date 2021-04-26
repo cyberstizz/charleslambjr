732fc6d9bf4f598883d9b1da21238c303a169f98
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-25 22:16:46 -0400

    added another description this one being from code wars

--- src/components/Challenges.js
+++ src/components/Challenges.js
@@ -8,7 +8,7 @@ const PrioritizePopup = 'Create a function prioritize that accepts an array and 
 
 const intersectionPopup = 'Construct a function intersection that compares input arrays and returns a new array with elements found in all of the inputs. ';
 
-const findTheOutlierPopup = '';
+const findTheOutlierPopup = 'You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.';
 
 const hardProblemPopup = '';
 
