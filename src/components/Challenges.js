ba0ef77766db9a386354fa5c457007783f87b65f
Charles Lamb <charles.lamb.dev@gmail.com>
2021-04-27 23:51:59 -0400

    commented out the last challenges variable because it was disrupting the deploy due to its nonuse

--- src/components/Challenges.js
+++ src/components/Challenges.js
@@ -10,7 +10,7 @@ const intersectionPopup = 'Construct a function intersection that compares input
 
 const findTheOutlierPopup = 'You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.';
 
-const hardProblemPopup = '';
+// const hardProblemPopup = '';
 
 
 const Challenges = () =>{      
