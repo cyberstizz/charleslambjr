3bc4b59fefe655f6f58ef19183675bb398748719
Charles Lamb <charles.lamb.dev@gmail.com>
2021-06-24 21:44:32 -0400

    changed slogan

--- src/components/About.js
+++ src/components/About.js
@@ -41,7 +41,7 @@ return (
         </div>
         <div id="aboutFooter">
 
-            <div id="footerSlogan">Lets talk about joining your team!</div>
+            <div id="footerSlogan">I have been practicing web development for 5 years!</div>
             <div id="footerDescription"> My goal is to be a member of a cutting edge team in our<br /> incredible industry. Whether it be desktop, mobile or front end I am addicted to growing and learning how to help people have a better experience</div>
             <div id="emailButton"></div>
 
