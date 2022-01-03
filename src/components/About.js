601461ed23dc72c0bb1f16c91af9b1236f26d6fe
charleslambdev <charleslambdev@penguin>
2022-01-03 01:53:59 -0500

    changed about me a bit on the footer slogan

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ return (
         <div id='aboutSkewedDov'></div>
         <div id="aboutHeader">About</div>
         <div id="aboutParagraph">Hi my name is Charles Lamb jr! I am a web developer and bootcamp graduate from Nyc now living in Atlanta Georgia.
-         I have been practicing web development for 5 years but started to pursue a career 2 years ago.
+         I have been practicing web development for 3 years but started to pursue a career 2 years ago.
           My goal is to be a member of a cutting edge team in our incredible industry. Whether it be desktop, mobile or front end I am addicted to growing and learning how to help people have a better experience on their devices.
            Javascript is the love of my life and I just love when she arrives wearing React, however I am open to seeing her in different styles.
             After a fairly lengthy career in the meretricious industry of high retail tech and real estate sales I have finally decided to switch careers and dedicate all of my energy and time into my true love...
@@ -41,7 +41,7 @@ return (
         </div>
         <div id="aboutFooter">
 
-            <div id="footerSlogan">I have been practicing web development for 5 years!</div>
+            <div id="footerSlogan">I have been practicing web development for 3 years!</div>
             <div id="footerDescription"> My goal is to be a member of a cutting edge team in our<br /> incredible industry. Whether it be desktop, mobile or front end I am addicted to growing and learning how to help users have a better experience</div>
             <div id="emailButton"></div>
 
