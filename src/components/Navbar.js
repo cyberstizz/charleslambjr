25bd0f3af5a349eee4fdbeff9a5be8517832c2f5
Charles <charles.lamb.dev@gmail.com>
2023-07-25 01:12:59 -0400

    added the newer resume to replace the old

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = () =>{
 
  <Link to='/' style={{ textDecoration: 'none' }}>  <div className="home">Home</div></Link> 
   <Link to='/Projects' style={{ textDecoration: 'none' }}> <div className="projects">Projects</div></Link>
-  <Link to='/challenges' style={{ textDecoration: 'none' }}> <div className="challenges">Challenges</div></Link>
+  <Link to='/challenges' style={{ textDecoration: 'none' }}> <div className="challenges">Coding</div></Link>
   <Link to='/about' style={{ textDecoration: 'none' }}> <div className="about">About</div></Link>
   {/* <Link to='/lamb' style={{ textDecoration: 'none' }}>  <div className="lamb">Lamb</div></Link> */}
   {/* <Link to='/journal' style={{ textDecoration: 'none' }}>  <div className="journal">Journal</div></Link> */}
