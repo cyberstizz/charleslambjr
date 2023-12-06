9e6db8943f6cb44df1149485f38deff37fd1e3fb
Charles <charles.lamb.dev@gmail.com>
2023-12-06 16:13:32 -0500

    added more test to the landing page

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -7,7 +7,7 @@ const Splash = () =>{
     return (
         <div className='splashDiv'>
         <h2 data-text="coming soon">
-       coming soon
+       Charles Lamb Jr V2 coming December 10th
        </h2>
 
        <Link to="./home"><h3>visit old website</h3></Link>
