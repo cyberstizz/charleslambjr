8832e8e2cb8f857476b205b075d0a1a602fec1d6
Charles <charles.lamb.dev@gmail.com>
2023-12-05 21:10:36 -0500

    added a div to enxapsulate the h2

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -5,9 +5,11 @@ import { Link } from 'react-router-dom';
 
 const Splash = () =>{
     return (
+        <div className='splashDiv'>
         <h2 data-text="coming soon">
        coming soon
        </h2>
+       </div>
     )
 };
 
