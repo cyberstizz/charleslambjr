763c80e5076c7c0104aa72a219562bd5c22921eb
Charles <charles.lamb.dev@gmail.com>
2023-12-17 15:37:06 -0500

    added some outlining for the elements that i will be creating

--- src/components/SampleCard.js
+++ src/components/SampleCard.js
@@ -4,7 +4,9 @@ import './SampleCard.css';
 
 const SampleCard = (cardData) => (
   <div className="cardOutline">
-    
+    <header className='projectTechnologiesOutline'></header>
+    <img className='cardImg' src={cardData.image} />
+    <footer className='cardFooter'></footer>
 
 
   </div>
